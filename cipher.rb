88d0d7220858c65ebda4edf307b53c1ebc2787b6
L. Soai-Van <leo.soaivan@gmail.com>
2016-12-23 23:32:19 -0500

    Add first draft of index.erb

--- cipher.rb
+++ cipher.rb
@@ -1,1 +1,25 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @translation = cipher(params[:message], params[:key])
+  erb :index, :locals => { :translation => @translation }
+end
+
+def cipher(str, key)
+  key %= 26
+
+  ary = str.split(//).map! do |char|
+    if /[^a-zA-Z]/.match(char)
+      char
+    else
+      beginning = (/[a-z]/.match(char) ? "a" : "A" )
+      ((char.ord + key - beginning.ord) % 26 + beginning.ord).chr
+    end
+  end
+  ary.join
+end
