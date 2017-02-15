57bd7851e6a29c8cb1ec69601b6ea3fd4d2f1a3b
L. Soai-Van <leo.soaivan@gmail.com>
2017-02-14 22:29:59 -0500

    Add minor edit to script.rb

--- script.rb
+++ script.rb
@@ -6,17 +6,11 @@ get '/' do
 end
 
 post '/' do
-  @translation = Cipher.new(transfer_params(params)).translate_all
+  @translation = Cipher.new(params).translate_all
   erb :index, :locals => { :translation => @translation }
 end
 
 post '/clear' do
   @translation = nil
   erb :index, :locals => { :translation => @translation }
-end
-
-private
-
-def transfer_params(params)
-  {message: params[:message], key: params[:key].to_i}
 end
